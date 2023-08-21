95e538e4b46d355c9221901f47ed6ed814897383
Antonio <antoniosansotta@gmail.com>
2023-08-21 09:07:27 -0400

    updating homescreen categories

--- src/routes/education/terms/+page.svelte
+++ src/routes/education/terms/+page.svelte
@@ -2,22 +2,22 @@
     import { Autocomplete } from '@skeletonlabs/skeleton';
     import type { AutocompleteOption } from '@skeletonlabs/skeleton';
 
-let inputDemo = '';
+	let inputDemo = '';
 
 
-const flavorOptions: AutocompleteOption[] = [
-	{ label: 'Vanilla', value: 'vanilla', keywords: 'plain, basic', meta: { healthy: false } },
-	{ label: 'Chocolate', value: 'chocolate', keywords: 'dark, white', meta: { healthy: false } },
-	{ label: 'Strawberry', value: 'strawberry', keywords: 'fruit', meta: { healthy: true } },
-	{ label: 'Neapolitan', value: 'neapolitan', keywords: 'mix, strawberry, chocolate, vanilla', meta: { healthy: false } },
-	{ label: 'Pineapple', value: 'pineapple', keywords: 'fruit', meta: { healthy: true } },
-	{ label: 'Peach', value: 'peach', keywords: 'fruit', meta: { healthy: true } }
-];
+	const flavorOptions: AutocompleteOption[] = [
+		{ label: 'Vanilla', value: 'vanilla', keywords: 'plain, basic', meta: { healthy: false } },
+		{ label: 'Chocolate', value: 'chocolate', keywords: 'dark, white', meta: { healthy: false } },
+		{ label: 'Strawberry', value: 'strawberry', keywords: 'fruit', meta: { healthy: true } },
+		{ label: 'Neapolitan', value: 'neapolitan', keywords: 'mix, strawberry, chocolate, vanilla', meta: { healthy: false } },
+		{ label: 'Pineapple', value: 'pineapple', keywords: 'fruit', meta: { healthy: true } },
+		{ label: 'Peach', value: 'peach', keywords: 'fruit', meta: { healthy: true } }
+	];
 				
 
-function onFlavorSelection(event: any): void {
-	inputDemo = event.detail.label;
-}
+	function onFlavorSelection(event: any): void {
+		inputDemo = event.detail.label;
+	}
 				
 </script>
 
