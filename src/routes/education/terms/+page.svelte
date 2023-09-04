6a502c85fd9d228b1516b4513c6f9262ff4167e0
Antonio <antoniosansotta@gmail.com>
2023-09-04 08:54:24 -0400

    cleaned up table code from terms page

--- src/routes/education/terms/+page.svelte
+++ src/routes/education/terms/+page.svelte
@@ -5,9 +5,6 @@
 
 	// import { Autocomplete } from '@skeletonlabs/skeleton';
 	// import type { AutocompleteOption } from '@skeletonlabs/skeleton';
-	// import { Table } from '@skeletonlabs/skeleton';
-	// import type { TableSource } from '@skeletonlabs/skeleton';
-	// import { tableMapperValues } from '@skeletonlabs/skeleton';
 	let inputDemo = '';
 
 	// const flavorOptions: AutocompleteOption[] = [
@@ -59,22 +56,6 @@
 		}
 	];
 
-	// console.log(categories);
-
-	// const tableSimple: TableSource = {
-	// 	// A list of heading labels.
-	// 	head: ['Name', 'SymbDefinitionol'],
-	// 	// The data visibly shown in your table body UI.
-	// 	body: tableMapperValues(sourceData, ['term', 'briefDefinition']),
-	// 	// Optional: The data returned when interactive is enabled and a row is clicked.
-	// 	meta: tableMapperValues(sourceData, ['term', 'briefDefinition'])
-	// 	// Optional: A list of footer labels.
-	// 	// foot: ['Total', '', '<code class="code">5</code>']
-	// };
-
-	// const loadTableData = (e: any) =>{
-	// 	console.log(e.detail)
-	// }
 </script>
 
 <input
@@ -91,5 +72,4 @@
 	</div>
 {/if} -->
 
-<!-- <Table source={tableSimple} class="p-5 table-interactive table-sort-asc" interactive={true} on:selected={loadTableData}/> -->
 <Categories {categories}/>
--- src/routes/education/terms/[termid]/+page.ts
+++ src/routes/education/terms/[termid]/+page.ts
@@ -2,7 +2,6 @@
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = ({ params }) => {
 
-    // console.log(params.termid)
     return {
         term: {
             id: params.termid
